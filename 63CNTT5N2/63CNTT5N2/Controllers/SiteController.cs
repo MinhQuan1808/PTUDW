dfab5559255e97b2d2e2db4208a0d1201006ffa3
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2023-09-23 20:00:52 +0700

    Init Project

---
+++ 63CNTT5N2/63CNTT5N2/Controllers/SiteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _63CNTT5N2.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Site
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
