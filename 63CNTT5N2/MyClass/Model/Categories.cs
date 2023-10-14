aac38b3e6bf287ee770eb66712d770b25b4e8564
MinhQuan1808 <145845509+MinhQuan1808@users.noreply.github.com>
2023-10-14 17:15:40 +0700

    Init Project

--- 63CNTT5N2/MyClass/Model/Categories.cs
+++ 63CNTT5N2/MyClass/Model/Categories.cs
@@ -12,31 +12,43 @@ namespace MyClass.Model
     public class Categories
     {
         [Key]
+        [Display(Name ="Id")]
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Tên sản loại sản phẩm")]
         public string Name { get; set; }
 
+        [Display(Name = "Tên rút gọn")]
         public string Slug { get; set; }
 
+        [Display(Name = "Nút cha")]
         public int? ParentId { get; set; }
 
+        [Display(Name = "Sắp xếp")]
         public int? Order { get; set; }
 
         [Required]
+        [Display(Name = "Mô tả")]
         public string MetaDesc { get; set; }
 
         [Required]
+        [Display(Name = "Từ Khóa")]
         public string MetaKey { get; set; }
 
+        [Display(Name = "Người tạo")]
         public int CreatedBy { get; set; }
 
+        [Display(Name = "Ngày tạo")]
         public DateTime CreatedAt { get; set; }
 
+        [Display(Name = "Người cập nhật")]
         public int UpdateBy { get; set; }
 
+        [Display(Name = "Ngày cập nhật")]
         public DateTime UpdateAt { get; set; }
 
+        [Display(Name = "Trạng thái")]
         public int Status { get; set; }
 
     }
